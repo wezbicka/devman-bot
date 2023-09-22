0456cad4f4a987f6350881873a61257482551f8a
Polina <75391420+wezbicka@users.noreply.github.com>
2023-09-22 12:49:53 +0300

    Create tgbot

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # devman-bot
  
+Telegram bot for sending notifications about verification of works on [dvmn.org](https://dvmn.org/modules).
+## How to install
+```
+pip install -r requirements.txt
+```
+* **Run on local machine**
+
+Create .env file with environment variables:
+```
+token_devman=<TOKEN_DEVMAN>
+token_bot=<TOKEN_BOT>
+chat_id=<CHAT_ID>
+```
+## Usage
+```
+python main.py
+```
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import os
-import logging_config
 import logging
 
 import requests
 from dotenv import load_dotenv
+
+import telegram
 
 
 logger = logging.getLogger(__name__)
@@ -11,12 +12,19 @@ logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
     load_dotenv()
+    tg_token = os.environ["TG_TOKEN"]
+    chat_id = os.environ["CHAT_ID"]
+    bot = telegram.Bot(token=tg_token)
+
     devman_api_token = os.environ["DEVMAN_API_TOKEN"]
     url = "https://dvmn.org/api/long_polling/"
     headers = {
         'Authorization': f"Token {devman_api_token}"
     }
-    timestamp=0
+    timestamp = ""
+    positive_message = "Преподавателю всё понравилось, \
+        можно приступать к следущему уроку!"
+    negative_message = "К сожалению, в работе нашлись ошибки."
     while True:
         params = {"timestamp": timestamp}
         try:
@@ -27,15 +35,18 @@ if __name__ == "__main__":
                 params=params,
             )
             response.raise_for_status()
-            print(response.json())
-            timestamp = response.json()["new_attempts"][0]["timestamp"]
-            
+            query = response.json()
+            logger.info(query)
+            attempt_details = query["new_attempts"][0]
+            message = negative_message if attempt_details["is_negative"] else positive_message
+            timestamp = attempt_details["timestamp"]
+            answer = """У вас проверили работу "{}"
+{}
+
+{}""".format(attempt_details["lesson_title"], attempt_details["lesson_url"], message)
+            bot.send_message(chat_id=chat_id, text=answer)
         except (
-            requests.exceptions.ReadTimeout,
-            requests.exceptions.ConnectionError
+            requests.exceptions.ConnectionError,
+            requests.exceptions.ReadTimeout
         ) as exception:
             logger.error("Failed request %s", exception)
-            # timestamp = response.json()["timestamp_to_request"]
-        print(timestamp)
-        # for message in response:
-        #     bot.answer(message)
